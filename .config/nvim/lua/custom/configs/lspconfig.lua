656f60ce73a82e0f863f8f2698f158aaf6cadae0
Francisco Alcalde <francisco.alcalde@alcalde.com.ar>
2024-03-16 17:02:11 -0300

    Agregadas mis configs de nvim. (Tuve que eliminar el repo de nvchad, espero que no traiga problemas...)

---
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,12 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+-- local cmd = { "rustup", "run", "stable", "rust-analyzer", }
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+
+lspconfig.r_language_server.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
